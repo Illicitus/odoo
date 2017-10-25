a33df1ede418d7b896ed692d6397a6f608ac8e0b
illicitus <suenoyactuar@gmail.com>
2017-10-25 14:37:32 +0300

    Add correct is_tested function.

--- odoo/test_v2/models/models.py
+++ odoo/test_v2/models/models.py
@@ -7,7 +7,14 @@ class ResPartners(models.Model):
     _name = 'res.partners'
 
     name = fields.Char()
-    is_tested = fields.Boolean()
+    is_tested = fields.Boolean(default=False, compute='check_is_tested')
+
+    @ api.depends('name')
+    def check_is_tested(self):
+        # If partner name already have relation with test, field is_tested will be True.
+
+        if self.env['res.partners'].search([('name', '=', '{0}'.format(self.name))]):
+            self.is_tested = True
 
 
 class Test(models.Model):
@@ -32,9 +39,8 @@ class TestSession(models.Model):
 
         for record in self:
             if not (record.start_date and record.end_date):
-                    continue
+                continue
 
             start_date = fields.Datetime.from_string(record.start_date)
             end_date = fields.Datetime.from_string(record.end_date)
             record.duration = (end_date - start_date).days + 1
-
